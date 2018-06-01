9ab245b430a4c059d67d036e5d7d6546e00a3136
iEzreal <747141623@qq.com>
2018-06-01 17:33:27 +0800

    jsp

---
+++ src/main/java/com/liu/model/User.java
@@ -0,0 +1,23 @@
+package com.liu.model;
+
+public class User {
+	private String loginName;
+	private String password;
+
+	public String getLoginName() {
+		return loginName;
+	}
+
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
